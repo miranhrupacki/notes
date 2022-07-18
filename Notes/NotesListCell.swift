52e9bbddc698495d9d1be0052c937e154cfd7d3b
mhrupacki <miran.hrupacki@barrage.net>
2022-07-18 20:48:56 +0200

    setup list of notes and details

---
+++ Notes/NotesListCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotesListCell.swift
+//  Notes
+//
+//  Created by Miran Hrupački on 18.07.2022..
+//
+
+import UIKit
+
+class NotesListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
